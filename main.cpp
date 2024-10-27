4af9e6ec0ded8a625b4e72b240e2dfba458074d3
Fabian Schillig <fabian@libf.de>
2024-10-27 15:29:36 +0100

    test4

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ int main() {
                 break;
             }
 
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
 
 
